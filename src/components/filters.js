e487afc92ad8493cc2d108507ff9c0c1f7532e2d
ryanKrumbholz <krumbholz98@gmail.com>
2020-03-22 12:35:29 -0400

    Filters section html and css formatting

--- src/components/filters.js
+++ src/components/filters.js
@@ -4,7 +4,119 @@ import './filters.css';
 const filters = props => {
   return (
       <div className="Filters">
-        <p>Filters</p>
+        <ul class="DatesFiltersList">
+          <li>
+            <form action="">
+              <label for="from">From:</label>
+              <input type="date" id="" name=""/>
+              <input type="submit"/>
+            </form>
+          </li>
+          <li>
+            <form action="">
+              <label for="from">From:</label>
+              <input type="date" id="" name=""/>
+              <input type="submit"/>
+            </form>
+          </li>
+        </ul>
+        <ul class="TypeFiltersList">
+          <li>
+            <form onSubmit="">
+              <label>
+                <input type="text" value="Merchant" onChange="" />
+              </label>
+              <input type="submit" value="Submit" />
+          </form>
+        </li>
+          <li><button>idk rn</button></li>
+          <li>
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </li>
+          <li>
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </li>
+        </ul>
+        <ul class="CategoryTagList">
+          <li>
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </li>
+          <li>
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </li>
+          <li>
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </li>
+        </ul>
+        <ul class="StatusList">
+          <li>
+            <label class="container">Unreported
+              <input type="checkbox" checked="checked"/>
+              <span class="checkmark"></span>
+            </label>
+          </li>
+          <li>
+            <label class="container">Open
+              <input type="checkbox" checked="checked"/>
+              <span class="checkmark"></span>
+            </label>
+          </li>
+          <li>
+            <label class="container">Processing
+              <input type="checkbox" checked="checked"/>
+              <span class="checkmark"></span>
+            </label>
+          </li>
+          <li>
+            <label class="container">Approved
+              <input type="checkbox" checked="checked"/>
+              <span class="checkmark"></span>
+            </label>
+          </li>
+          <li>
+            <label class="container">Reimbursed
+              <input type="checkbox" checked="checked"/>
+              <span class="checkmark"></span>
+            </label>
+          </li>
+          <li>
+            <label class="container">Closed
+              <input type="checkbox" checked="checked"/>
+              <span class="checkmark"></span>
+            </label>
+          </li>
+          <li>
+            <label class="container">Deleted
+              <input type="checkbox" checked=""/>
+              <span class="checkmark"></span>
+            </label>
+          </li>
+        </ul>
       </div>
   )
 }
