7e6b7c032ea1eaf1339562924ee27ea0b0667093
ryanKrumbholz <krumbholz98@gmail.com>
2020-03-30 14:54:41 -0400

    Tied express intoo react

--- expensify-client /client/src/App.js
+++ expensify-client /client/src/App.js
@@ -4,11 +4,26 @@ import ExpensesBody from './components/expenses_body'
 import './App.css';
 
  class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+      this.callAPI();
+  }
   render () {
     return(
       <div class="App">
         <Sidebar/>
         <ExpensesBody /> {/* will add function to switch between reports and this */}
+        <p className="App-intro">;{this.state.apiResponse}</p>
       </div>
     );
   }
