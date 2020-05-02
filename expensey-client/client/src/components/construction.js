6b123cb3dc79526d2143746635b03f981890c5ce
ryanKrumbholz <krumbholz98@gmail.com>
2020-05-02 14:56:16 -0400

    Changed h3 to h2

--- expensey-client/client/src/components/construction.js
+++ expensey-client/client/src/components/construction.js
@@ -4,7 +4,7 @@ import './construction.css';
 const construction = props => {
   return (
       <div className="construction">
-        <h3>Site currently under construction. Please check back at a later date!</h3>
+        <h2>Site currently under construction. Please check back at a later date!</h2>
         <img src="https://media3.giphy.com/media/3ohhwGBJz701RyAxk4/source.gif"></img>
       </div>
   )
