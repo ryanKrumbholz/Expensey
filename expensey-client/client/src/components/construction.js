fe95b3e1c1c97beae060e4abe25f75e841c427ba
ryanKrumbholz <krumbholz98@gmail.com>
2020-05-02 14:54:17 -0400

    Added gif to construction page

--- expensey-client/client/src/components/construction.js
+++ expensey-client/client/src/components/construction.js
@@ -4,7 +4,8 @@ import './construction.css';
 const construction = props => {
   return (
       <div className="construction">
-        <h1>Site currently under construction. Please check back at a later date!</h1>
+        <h3>Site currently under construction. Please check back at a later date!</h3>
+        <img src="https://media3.giphy.com/media/3ohhwGBJz701RyAxk4/source.gif"></img>
       </div>
   )
 }
