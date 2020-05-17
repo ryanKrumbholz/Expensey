e2ad1f7b1798cd15a4e5b79a0a7a756b99ab861b
ryanKrumbholz <krumbholz98@gmail.com>
2020-05-17 15:01:53 -0400

    Revert last change

--- expensey-client/client/src/App.js
+++ expensey-client/client/src/App.js
@@ -282,7 +282,6 @@ componentDidUpdate() {
     if(this.getCookie("status") == "true"){
       return(
         <div class="App">
-        <h1>Site currently under construction</h1>
         {this.getViewMode()}
           {/* Construction is temp for production*/}
           {/* <Construction/> */}
